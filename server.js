795a96923545821d4c133f2eb604c7d3cb3f28ec
Amelia Kuang <qianyi.kuang@gmail.com>
2019-06-18 21:09:16 -0700

    Add config

--- server.js
+++ server.js
@@ -10,62 +10,101 @@ app.use(bodyParser.json());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
+var loca_db_uri = "mongodb://localhost:27017/test";
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+mongodb.MongoClient.connect(process.env.MONGODB_URI || loca_db_uri, { useNewUrlParser: true }, function (err, client) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
+    // Save database object from the callback for reuse.
+    db = client.db();
+    console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
 });
 
 // REPORTS API ROUTES BELOW
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
     console.log("ERROR: " + reason);
-    res.status(code || 500).json({"error": message});
-  }
-  
-  /*  "/api/reports"
-   *    GET: finds all reports
-   *    POST: creates a new report
-   */
-  
-  app.get("/api/reports", function(req, res) {
-    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    res.status(code || 500).json({ "error": message });
+}
+
+/*  "/api/reports"
+ *    GET: finds all reports
+ *    POST: creates a new report
+ */
+
+app.get("/api/reports", function (req, res) {
+    db.collection(REPORTS_COLLECTION).find({}).toArray(function (err, docs) {
         if (err) {
-          handleError(res, err.message, "Failed to get contacts.");
+            handleError(res, err.message, "Failed to get reports.");
         } else {
-          res.status(200).json(docs);
+            res.status(200).json(docs);
         }
     });
-  });
-  
-  app.post("/api/reports", function(req, res) {
-  
+});
+
+app.post("/api/reports", function (req, res) {
+    var newReport = req.body;
+    newReport.createDate = new Date();
+
+    if (!req.body.name) {
+        handleError(res, "Invalid user input", "Must provide a name.", 400);
+    } else {
+        db.collection(REPORTS_COLLECTION).insertOne(newReport, function (err, doc) {
+            if (err) {
+                handleError(res, err.message, "Failed to create new report.");
+            } else {
+                res.status(201).json(doc.ops[0]);
+            }
+        });
+    }
+});
+
+/*  "/api/reports/:id"
+ *    GET: find report by id
+ *    PUT: update report by id
+ *    DELETE: deletes report by id
+ */
+
+app.get("/api/reports/:id", function (req, res) {
+    db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contact");
+        } else {
+            res.status(200).json(doc);
+        }
     });
-  
-  /*  "/api/reports/:id"
-   *    GET: find report by id
-   *    PUT: update report by id
-   *    DELETE: deletes report by id
-   */
-  
-  app.get("/api/reports/:id", function(req, res) {
-  });
-  
-  app.put("/api/reports/:id", function(req, res) {
-  });
-  
-  app.delete("/api/reports/:id", function(req, res) {
-  });
+});
+
+app.put("/api/reports/:id", function (req, res) {
+    var updateDoc = req.body;
+    delete updateDoc._id;
+
+    db.collection(CONTACTS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to update contact");
+        } else {
+            updateDoc._id = req.params.id;
+            res.status(200).json(updateDoc);
+        }
+    });
+});
+
+app.delete("/api/reports/:id", function (req, res) {
+    db.collection(CONTACTS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
+        if (err) {
+            handleError(res, err.message, "Failed to delete contact");
+        } else {
+            res.status(200).json(req.params.id);
+        }
+    });
+});
